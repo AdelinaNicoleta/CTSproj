8ff764377c5c850b5731c00c3fd14ac11341920e
AdelinaNicoleta <ciocoiuadelina13@stud.ase.ro>
2016-05-20 00:41:21 +0300

    Add files via upload

---
+++ WinS_Tests/DiscoverRomaniaTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WinS_RomanianTrips.Common;
+using System.IO;
+using System.Data;
+
+namespace WinS_Tests
+{
+    [TestClass]
+    public class DiscoverRomaniaTests
+    {
+
+    }
+}
